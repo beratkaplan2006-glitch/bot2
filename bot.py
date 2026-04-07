5e112a53bc132357880bbb1d89f142c12d53c77e
beratkaplan2006-glitch <beratkaplan2006@gmail.com>
2026-04-07 04:30:38 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -17,7 +17,7 @@ RSS_FEEDS = [
     "https://feeds.finance.yahoo.com/rss/2.0/headline?s=market&region=US&lang=en-US"
 ]
 
-THRESHOLD_RATIO = 0.5
+THRESHOLD_RATIO = 0.1
 TIME_WINDOW = 2  # dakika
 SCAN_INTERVAL = 15  # saniye
 AI_THRESHOLD = 50
